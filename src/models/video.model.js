7a1673538c0b3d5d354352a1cc6f9ddca3356e08
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-03-04 22:24:29 +0530

    Models added

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -31,7 +31,7 @@ const videoSchema = new Schema(
             type: Boolean,
             default: true,
         },
-        owner: {
+        owner: { 
             type: Schema.Types.ObjectId,
             ref: "User"
         }
